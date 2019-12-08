33fd34cfefa9cb446b1b56415d6efae9dc19b130
Tasha-Sh <46695598+Tasha-Sh@users.noreply.github.com>
2019-12-08 22:13:26 +0200

    'l'l

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tastCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пользователь вводит два числа. Вывести на экран большее
+            Console.WriteLine("Enter first number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            if (firstNumber> secondNumber)
+            {
+                Console.WriteLine("Bigger is:"+firstNumber);
+            }
+            else
+            {
+                Console.WriteLine("Bigger is:" + secondNumber);
+            }
+            Console.ReadKey();
+        }
+    }
+}
